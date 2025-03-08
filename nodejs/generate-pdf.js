e1a61d95394d92a4e832bfc27d35c4a2f773d473
Ibrahim Issa <al3almyibrahim@gmail.com>
2025-03-08 21:00:29 +0700

    Auto

--- nodejs/generate-pdf.js
+++ nodejs/generate-pdf.js
@@ -3,10 +3,7 @@ import path from "path";
 
 async function generatePdf(html, outputFileName) {
     try {
-        // const browser = await puppeteer.launch();
-        const browser = await puppeteer.launch({
-            executablePath: '/home/wilsoncl/.cache/puppeteer/chrome/linux-133.0.6943.141/chrome-linux64/chrome',
-        });
+        const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.setContent(html);
         await page.pdf({
