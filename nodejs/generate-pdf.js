ce133f4266acd50d142ef24017bb578be41a165a
Ibrahim Issa <al3almyibrahim@gmail.com>
2025-03-09 19:44:03 +0700

    Auto

--- nodejs/generate-pdf.js
+++ nodejs/generate-pdf.js
@@ -1,9 +1,10 @@
 import puppeteer from "puppeteer";
-import path from "path";
 
 async function generatePdf(html, outputFileName) {
     try {
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
         const page = await browser.newPage();
         await page.setContent(html);
         await page.pdf({
