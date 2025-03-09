2163338c09923f6de510b9a3da7d20c4dad4cb9a
Ibrahim Issa <al3almyibrahim@gmail.com>
2025-03-09 20:29:56 +0700

    Auto

--- routes/web.php
+++ routes/web.php
@@ -177,9 +177,23 @@ Route::middleware('auth')->group(function () {
 Route::get('public-pigeon/{pigeon}', [PigeonController::class, 'publicPigeon'])->name('pigeons.publicPigeon');
 
 Route::get('/test', function () {
-    // Ensure the correct environment variables are set
-    $output = shell_exec('export PATH=$PATH:/www/server/nodejs/v20.10.0/bin && node -v');
+    $command = '/www/server/nodejs/v20.10.0/bin/node -v';
+    $descriptorspec = [
+        0 => ["pipe", "r"],  // stdin
+        1 => ["pipe", "w"],  // stdout
+        2 => ["pipe", "w"]   // stderr
+    ];
+    $process = proc_open($command, $descriptorspec, $pipes);
+
+    if (is_resource($process)) {
+        $output = stream_get_contents($pipes[1]);
+        fclose($pipes[1]);
+        fclose($pipes[2]);
+        proc_close($process);
+    }
+
     dd($output);
 });
+
 
 require __DIR__ . '/auth.php';
